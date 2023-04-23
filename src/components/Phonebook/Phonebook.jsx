99a8c39df982e253fc3cdf1f17e22d354066e36f
MishaStryzhka <misha.strizhcko@gmail.com>
2023-04-23 17:14:31 +0200

    Update task 1

---
+++ src/components/Phonebook/Phonebook.jsx
@@ -0,0 +1,81 @@
+import { Component } from "react";
+import { nanoid } from "nanoid";
+
+import FormContacts from "components/FormContacts/FormContacts";
+import FormFind from "components/FormFind/FormFind";
+import css from './Phonebook.module.css';
+
+const KEYCONTACTS = "contacts";
+
+class Phonebook extends Component {
+    state = {
+        contacts: [],
+        filter: ''
+    }
+
+    componentDidMount = () => {
+        this.setState({ contacts: JSON.parse(localStorage.getItem(KEYCONTACTS)) })
+    }
+
+
+    componentDidUpdate = (prevProps, prevState) => {
+        if (prevState.contacts.length !== this.state.contacts.length) {
+            console.log(this.state.contacts);
+            localStorage.setItem(KEYCONTACTS, JSON.stringify(this.state.contacts));
+        }
+    }
+
+    onSubmit = (user) => {
+        if (this.state.contacts.find(contact => {
+            const normalizeUser = user.name.toLowerCase();
+            return contact.name.toLowerCase() === normalizeUser
+        })) { alert("Даний контакт вже є в телефонній") }
+        else {
+            const newUser = { id: nanoid(), ...user }
+            return this.setState((prefState) => ({ ...prefState, contacts: [...prefState.contacts, newUser] }))
+        }
+    }
+
+    handleChange = e => {
+        this.setState({ [e.target.name]: e.target.value });
+    };
+
+    handleRemove = e => {
+        this.setState({ contacts: this.state.contacts.filter(value => value.id !== e.currentTarget.parentNode.id) })
+    }
+
+    getVizibleContacts = () => {
+        const { contacts, filter } = this.state;
+
+        const normalizedFilter = filter.toLowerCase();
+
+        return contacts.filter(contact => contact.name.toLowerCase().includes(normalizedFilter))
+
+    }
+
+    render() {
+        return (
+            <>
+                <h1 className={css.title}>Phonebook</h1>
+
+                <FormContacts onSubmit={this.onSubmit} name />
+
+                <h2 className={css.title}>Contacts</h2>
+
+                <FormFind handleChange={this.handleChange} value={this.state.filter} />
+
+                <ul className={css.contactsList}>
+                    {this.getVizibleContacts().map((contact) =>
+                        <li className={css.item} key={contact.id} id={contact.id}>
+                            {contact.name} {contact.number}
+                            <button type="button" className="btn-close" aria-label="Close" onClick={this.handleRemove}></button>
+                        </li>
+                    )}
+                </ul>
+            </>
+        );
+    };
+
+}
+
+export default Phonebook;
